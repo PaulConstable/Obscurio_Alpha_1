c95a74c949e5c299934e7ea0bc02ebdcd3419c75
K1614994 <K1614994@users.noreply.github.com>
2019-02-02 00:29:06 +0000

    LightGameStart

---
+++ Obscurio/Assets/Scripts/LightGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightGame : MonoBehaviour {
+
+    SpriteRenderer button;
+    public Camera cam;
+    public Sprite but;
+
+// Use this for initialization
+void Start () {
+        button = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray click = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit connected;
+
+            if(Physics.Raycast(click, out connected))
+            {
+                //Change Colour of sprite 
+                button.color = Color.yellow;
+            }
+        }
+
+
+	}
+}
+
