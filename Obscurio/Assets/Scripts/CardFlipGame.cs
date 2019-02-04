4a889b8b31a2a97a3ae12f014782a7b3e07c5e37
K1614994 <K1614994@users.noreply.github.com>
2019-02-04 17:34:43 +0000

    CardFlipGame

---
+++ Obscurio/Assets/Scripts/CardFlipGame.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardFlipGame : MonoBehaviour {
+
+    public static bool stay = false;
+
+    [SerializeField] private int state;
+    [SerializeField] private int cardValue;
+    [SerializeField] private bool initial = false;
+
+    private Sprite cardBack;
+    private Sprite cardFront;
+
+    private GameObject manager;
+
+
+
+	// Use this for initialization
+	void Start () {
+        state = 1;
+        manager = GameObject.FindGameObjectWithTag("Manager");
+	}
+
+    public void createGraphics()
+    {
+        cardBack = manager.GetComponent<GameManager>().getcardBack();
+        cardFront = manager.GetComponent<GameManager>().getcardFront(cardValue);
+
+        flipCard();
+    }
+	
+    public void flipCard()
+    {
+        if (state == 1)
+        {
+            state = 0;
+        }
+        else if (state ==0)
+        {
+            state = 1;
+        }
+            
+                
+        if (state == 0 && !stay)
+            GetComponent<Image>().sprite = cardBack;
+        else if (state == 1 && !stay)
+            GetComponent<Image>().sprite = cardFront;
+    }
+
+    public int cardValues
+    {
+        get { return cardValue; }
+        set { cardValue = value; }
+    }
+
+    public int states
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+    public bool initialise
+    {
+        get { return initial; }
+        set { initial = value; }
+    }
+
+    public void falsecheck()
+    {
+        StartCoroutine(pause ());
+    }
+
+    IEnumerator pause()
+    {
+        yield return new WaitForSeconds(1);
+        if (state == 0)
+        {
+            GetComponent<Image>().sprite = cardBack;
+        }
+        else if(state ==1)
+        {
+            GetComponent<Image>().sprite = cardFront;
+            stay = false;
+        }
+    }
+    
+    
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Obscurio/Assets/Scripts/GameManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Sprite[] cardFaces;
+    public Sprite cardBack;
+    public GameObject[] cards;
+
+    private bool initial = false;
+    private int matches = 2;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!initial)
+            initialiseCards();
+
+        if (Input.GetMouseButtonUp(0))
+            checkCards();
+	}
+
+    void initialiseCards()
+    {
+        for (int i =0; i<2; i++)
+        {
+            for (int p =1; p<11; p++)
+            {
+                bool test = false;
+                int choice = 0;
+                while(!test)
+                {
+                    choice = Random.Range(0, cards.Length);
+                    test = !(cards[choice].GetComponent<CardFlipGame>().initialise);
+                }
+                cards[choice].GetComponent<CardFlipGame>().cardValues = p;
+                cards[choice].GetComponent<CardFlipGame>().initialise = true;
+            }
+        }
+
+        foreach (GameObject c in cards)
+            c.GetComponent<CardFlipGame>().createGraphics();
+
+        if (!initial)
+            initial = true;
+    }
+
+    public Sprite getcardBack()
+    {
+        return cardBack;
+    }
+
+    public Sprite getcardFront(int p)
+    {
+        return cardFaces[p - 1];
+    }
+
+
+void checkCards()
+    {
+        List<int> c = new List<int>();
+
+        for (int i=0; i<cards.Length; i++)
+        {
+            if (cards[i].GetComponent<CardFlipGame>().states == 1)
+                c.Add(i);
+        }
+
+        if (c.Count == 2)
+            cardComparison(c);
+    }
+
+    void cardComparison(List<int> c)
+    {
+        CardFlipGame.stay = true;
+
+        int x = 0;
+
+        if (cards [c [0]].GetComponent<CardFlipGame>().cardValues == cards [c [1]].GetComponent<CardFlipGame>().cardValues)
+        {
+            x = 2;
+            matches--;
+            if (matches==0)
+            {
+                Debug.Log("matches complete");
+            }
+        }
+
+        for(int i=0; i<c.Count; i++)
+        {
+            cards[c [i]].GetComponent<CardFlipGame>().states = x;
+            cards[c[i]].GetComponent<CardFlipGame>().falsecheck();
+        }
+    }
+
+}
