f06b40faa69aa5ebbd87fb3335bebfa3f27bd9fb
PaulConstable <PaulConstable@users.noreply.github.com>
2019-02-18 17:20:36 +0000

    TriggerSwitch

---
+++ Obscurio/Assets/DisableCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCollider : MonoBehaviour {
+
+    [SerializeField] Collider2D Collider;
+    [SerializeField] GameObject Collider2;
+    private bool Hide = false;
+    // Use this for initialization
+    void Start () {
+        Collider.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Hide == true)
+        {
+
+           
+
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject.Destroy(Collider2);
+        Collider.enabled = false;
+    }
+
+}
