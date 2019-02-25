5c5b9215654d9f05b546241e241f134ca25cf28e
K1614994 <K1614994@users.noreply.github.com>
2019-02-25 17:21:40 +0000

    Basic UI interaction

--- Obscurio/Assets/Scripts/LibrarianTrigger.cs
+++ Obscurio/Assets/Scripts/LibrarianTrigger.cs
@@ -4,18 +4,55 @@ using UnityEngine;
 
 public class LibrarianTrigger : MonoBehaviour {
 
-    [SerializeField] GameObject librarianText;
     
-
-    void OnCollisionEnter2D(Collision2D collision)
+    private int speechCheck = 1;
+    [SerializeField] public GameObject[] texts;
+    
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
-        if(collision.gameObject.tag == "Player")
+        //Setting the first UI
+        if (collision.gameObject.tag == "Player" && speechCheck == 1)
         {
-            Destroy(collision.gameObject);
-            Debug.Log("1");
-            librarianText.SetActive(true);
             
+            texts[0].SetActive(true);
+        }
+
+        //Changing the UI
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space pressed");
+            texts[0].SetActive(false);
+            texts[1].SetActive(true);
+            speechCheck = 2;
+        }
+
+        //Keeping the 2nd UI if player leaves trigger and returns
+        if (collision.gameObject.tag == "Player" && speechCheck == 2)
+        {
+            texts[1].SetActive(true);
+        }
+
+    }
+
+    //Removing UI when player leaves the trigger zone
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && speechCheck == 1)
+        {
+            
+            texts[0].SetActive(false);
+        }
+        if (collision.gameObject.tag == "Player" && speechCheck == 2)
+        {
+            
+            texts[1].SetActive(false);
         }
     }
+
+    
+
+
+
+
+
 }
