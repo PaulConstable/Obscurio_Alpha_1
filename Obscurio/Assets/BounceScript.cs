fdbf55733945fb36c1bd6815e7c755e59b603a67
PaulConstable <PaulConstable@users.noreply.github.com>
2019-03-01 16:15:56 +0000

    Well Jump Mini Game

--- Obscurio/Assets/BounceScript.cs
+++ Obscurio/Assets/BounceScript.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class BounceScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
---
+++ Obscurio/Assets/VerticlePlatformMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticlePlatformMove : MonoBehaviour {
+
+    private bool dirRight = true;
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        if (dirRight)
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        else
+            transform.Translate(-Vector2.up * speed * Time.deltaTime);
+
+        if (transform.position.y >= 4.0f)
+        {
+            dirRight = false;
+        }
+
+        if (transform.position.y <= -4)
+        {
+            dirRight = true;
+        }
+    }
+}
+
+
