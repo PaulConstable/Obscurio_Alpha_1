64da5f032d269aa949fc4b7d4471f777977cef45
K1614994 <K1614994@users.noreply.github.com>
2019-03-24 22:17:44 +0000

    librarianBegin

--- Obscurio/Assets/Scripts/Library-Intro/LibrarianTrigger.cs
+++ Obscurio/Assets/Scripts/Library-Intro/LibrarianTrigger.cs
@@ -4,12 +4,45 @@ using UnityEngine;
 
 public class LibrarianTrigger : MonoBehaviour {
 
-    
-    
+    public GameObject[] librarianTexts;
+    public GameObject eButton;
+    public int librarianText;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            eButton.SetActive(true);
+
+           
+        }
+
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && collision.gameObject.tag == "Player" && librarianText == 0)
+        {
+            librarianTexts[0].SetActive(true);
+            eButton.SetActive(false);
+            librarianText = 1;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            eButton.SetActive(false);
+        }
+        if(collision.gameObject.tag == "Player" && librarianText == 1)
+        {
+            librarianTexts[0].SetActive(false);
+        }
+    }
+
 
     
-
-
 
 
 
