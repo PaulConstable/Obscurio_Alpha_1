c668b678b6d6afd1f3da3ee2026ee7f1a21734ba
K1614994 <K1614994@users.noreply.github.com>
2019-04-02 16:47:59 +0100

    WitchStuff

--- Obscurio/Assets/Sounds/Narration/NarrationSounds.cs
+++ Obscurio/Assets/Sounds/Narration/NarrationSounds.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class NarrationSounds : MonoBehaviour {
 
-    
+    public AudioClip speech1;
+    public AudioSource audio;
 
 
     public void Start()
     {
-        
+        audio.GetComponent<AudioSource>();
         
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag == "Player")
+        {
+            audio.PlayOneShot(speech1, 1f);
+        }
             
         
     }
