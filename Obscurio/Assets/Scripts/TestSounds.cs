1468eb70f5d99584b9a2d01e947d544a0b39aea6
PaulConstable <PaulConstable@users.noreply.github.com>
2019-04-02 21:59:17 +0100

    AudioTestButtons

---
+++ Obscurio/Assets/Scripts/TestSounds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSounds : MonoBehaviour {
+
+    [SerializeField] AudioSource NarrativeSound;
+    [SerializeField] AudioSource SoundEffectsSound;
+    [SerializeField] AudioSource MusicSound;
+
+    // Use this for initialization
+    void Start () {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void playNarrative()
+    {
+        NarrativeSound.Play(0);
+    }
+
+    public void playSoundEffects()
+    {
+        SoundEffectsSound.Play(0);
+    }
+
+    public void playMusic()
+    {
+        MusicSound.Play(0);
+    }
+}
