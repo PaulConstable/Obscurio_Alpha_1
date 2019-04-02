143238223a24e983504bb086517558f065af4f90
K1614994 <K1614994@users.noreply.github.com>
2019-04-02 18:10:51 +0100

    StartGameFunction

--- Obscurio/Assets/StartGame.cs
+++ Obscurio/Assets/StartGame.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public GameObject EButton;
+
+    public void OnTriggerStay2D(Collider2D collision)
     {
-        
+
+        EButton.SetActive(true);
 
 
+        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Library Scene");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        EButton.SetActive(false);
     }
 
 
