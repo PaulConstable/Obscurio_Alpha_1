f612aaae2a894f5fea804581597198d7c8ebacf9
PaulConstable <PaulConstable@users.noreply.github.com>
2019-04-02 16:44:47 +0100

    AudioMixersDone

---
+++ Obscurio/Assets/VolumeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class VolumeScript : MonoBehaviour {
+
+    public AudioMixer NarratorMixer;
+    public AudioMixer MusicMixer;
+    public AudioMixer SoundMixer;
+
+    public void SetNarratorLevel(float sliderValue)
+    {
+        NarratorMixer.SetFloat("NarratorVol", Mathf.Log10 (sliderValue) * 20);
+    }
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        MusicMixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetSoundLevel(float sliderValue)
+    {
+        SoundMixer.SetFloat("SoundEffectsVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
