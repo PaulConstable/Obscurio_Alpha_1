33947ccc4431e30d45cf5cf08f6249651cfc863f
PaulConstable <PaulConstable@users.noreply.github.com>
2019-04-03 19:45:01 +0100

    StoryBoards

---
+++ Obscurio/Assets/Scripts/Story/StoryPagesScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryPagesScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void loadLevel1()
+    {
+
+    }
+}
+// well game
+// level 1
+//level 2
